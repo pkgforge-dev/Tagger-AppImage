de136e89f57a01f5a9d2eea6ff6542f87f1cab5d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-17 08:49:54 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q tagger | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.nickvision.tagger.svg
+export DESKTOP=/usr/share/applications/org.nickvision.tagger.desktop
+export DEPLOY_OPENGL=1
+export STARTUPWMCLASS=tagger  # For Wayland, this is 'org.nickvision.tagger', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/tagger
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
