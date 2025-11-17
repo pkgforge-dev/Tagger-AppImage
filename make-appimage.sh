a84330beaab91d56fff9561a53e2dbf954d00d1f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-17 09:08:18 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,7 @@ export DEPLOY_DOTNET=1
 export STARTUPWMCLASS=tagger  # For Wayland, this is 'org.nickvision.tagger', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Deploy dependencies
-quick-sharun /usr/bin/tagger
+quick-sharun /usr/bin/tagger /usr/lib/org.nickvision.tagger
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
