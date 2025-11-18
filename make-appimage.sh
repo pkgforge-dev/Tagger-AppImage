556e71454f7d60aafc3f7e993630b969a3b9c62b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-18 09:40:07 +0100

    Update make-appimage.sh for formatting

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,9 @@ export DEPLOY_DOTNET=1
 export STARTUPWMCLASS=tagger  # For Wayland, this is 'org.nickvision.tagger', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-quick-sharun /usr/bin/org.nickvision.tagger /usr/lib/org.nickvision.tagger /usr/share/help/*/tagger
+quick-sharun /usr/bin/org.nickvision.tagger \
+             /usr/lib/org.nickvision.tagger \
+             /usr/share/help/*/tagger
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
